9b92db4bb894b3e51548b52a70fe6a11db54da3a
oraclerevolution <mac.n.drix@gmail.com>
2024-04-12 22:14:25 +0000

    project bootstrap

---
+++ constants/color.js
@@ -0,0 +1,10 @@
+export const orange = '#ff9500';
+export const green = {
+    normal: '#02B53A',
+    hover: '#D2FCFF'
+}
+export const blue = "#0055FF";
+export const red = {
+    normal: '#FF107E',
+    hover: '#FEE0E2'
+}
